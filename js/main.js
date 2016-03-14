35b7f93f1fc8067f28ae5e551597150d386b7e05
Elena Alexie <elena.coman33@yahoo.com>
2016-03-15 01:20:13 +0200

    Single page, vertically align adjustments

--- js/main.js
+++ js/main.js
@@ -8,17 +8,27 @@ $(window).on('resize', function() {
 function verticallyPositionTextBox() {
 	var box = $('.description.js-box');
 	box.each(function() {
+		var box = $(this);
 		var boxHeight = $(this).height();
 		var parent    = $(this).closest('.js-parent');
 		var parentHeight = parent.height();
+		var marginTop = '';
+
 		if(parentHeight <= $(window).height()) {
-			var marginTop = 0;
 			var intVal = Math.round(parseInt(parentHeight - boxHeight) / 2);
 			if(intVal >= 0) {
 				marginTop = intVal + 'px';
 			}
-			$(this).css('margin-top', marginTop);
 		}
+		if($(window).width() < 750) {
+			box.find('.js-paragraph').css('margin-top', marginTop);
+			box.find('.js-paragraph').css('margin-top', marginTop);
+			
+			box.find('.js-paragraph').addClass('dark-bg');
+		} else {
+			box.find('.js-paragraph').removeClass('dark-bg');
+		}
+		box.css('margin-top', marginTop);
 	});
 
 }
