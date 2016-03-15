e098edd160b7d04a1a8709888d3ac5591315e70b
Elena Alexie <elena.coman33@yahoo.com>
2016-03-16 00:42:15 +0200

    Single page, CSS and JS adjustments.

--- js/main.js
+++ js/main.js
@@ -1,34 +1,61 @@
 $(document).ready(function() {
 	verticallyPositionTextBox();
+
+	playPauseVideo();
+
+	// Known bug: Fix vh font-size on window resize
+	// causeRepaintsOn = $("h1, h2, h3, p");
 });
+
 $(window).on('resize', function() {
+  	// causeRepaintsOn.css("z-index", 1);
+
+  	// Known bug: Fix vh font-size on window resize
 	verticallyPositionTextBox();
 });
 
 function verticallyPositionTextBox() {
-	var box = $('.description.js-box');
+	var box = $('.parallax').find('.description.js-box');
 	box.each(function() {
-		var box = $(this);
-		var boxHeight = $(this).height();
-		var parent    = $(this).closest('.js-parent');
-		var parentHeight = parent.height();
-		var marginTop = '';
-
-		if(parentHeight <= $(window).height()) {
-			var intVal = Math.round(parseInt(parentHeight - boxHeight) / 2);
+		var box 		 	= $(this);
+		var boxHeight 	 	= box.height();
+		var parent    	 	= $('#'+box.find('.js-paragraph').data('parent-id'));
+		var sectionHeight 	= parent.height();
+		var spaceTop 	 	= '';
+		
+		if(box.find('ing').length) {
+			box.find('ing').load(function() {
+				var intVal = Math.round(parseInt(sectionHeight - boxHeight) / 2);
+				if(intVal >= 0) {
+					spaceTop = intVal + 'px';
+				}
+			});
+		} else {
+			var intVal = Math.round(parseInt(sectionHeight - boxHeight) / 2);
 			if(intVal >= 0) {
-				marginTop = intVal + 'px';
+				spaceTop = intVal + 'px';
 			}
 		}
-		if($(window).width() < 750) {
-			box.find('.js-paragraph').css('margin-top', marginTop);
-			box.find('.js-paragraph').css('margin-top', marginTop);
-			
-			box.find('.js-paragraph').addClass('dark-bg');
-		} else {
-			box.find('.js-paragraph').removeClass('dark-bg');
-		}
-		box.css('margin-top', marginTop);
+		box.css('padding-top', spaceTop);
 	});
 
+}
+
+function playPauseVideo() {
+	$('.js-play-video').on('click', function() {
+		var video = $('#'+$(this).data('video'));
+		alert(video);
+	    if(!video.is(':visible')){
+	        video[0].pause();
+	        video.fadeOut('normal', function(){
+	            video.fadeIn('normal', function(){
+	                video[0].play();
+	            });
+	        });
+	    }else{
+	        video[0].play();
+	    }
+	}, function(){
+	    video[0].pause();
+	});
 }
